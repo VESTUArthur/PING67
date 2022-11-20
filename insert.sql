8b1f14149cbebddf4af2544471d8d18f5c3ad7ed
VESTUArthur <vestuarthur@gmail.com>
2022-11-20 21:32:14 +0100

    Update yml

--- insert.sql
+++ insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO marks (student_id, mark, subject) VALUES ( 3, 23,  'FUCK-YOU');
+INSERT INTO marks (student_id, mark, subject) VALUES ( 3, 23,  'NEW-ITEM');
 INSERT INTO marks (student_id, mark, subject) VALUES ( 4, 56,  'Planetary Geography');
 INSERT INTO marks (student_id, mark, subject) VALUES ( 9, 77,  'Foreign Evolutionary Biology');
 INSERT INTO marks (student_id, mark, subject) VALUES ( 6, 83,  'Intergallactic Relations');
